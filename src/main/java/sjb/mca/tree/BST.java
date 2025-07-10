4687ab6bed1841d0d26e0900d611869658ba41a9
Bhuvankumark <bhuvangowda2726@gmail.com>
2025-07-10 15:39:42 +0530

    Tree Traversal

---
+++ src/main/java/sjb/mca/tree/BST.java
@@ -0,0 +1,78 @@
+package sjb.mca.tree;
+
+public class BST {
+    TreeNode root;
+
+    void insert(int value) {
+        root = insertNode(root, value);
+    }
+
+    TreeNode insertNode(TreeNode node, int value) {
+        if (node == null) {
+            return new TreeNode(value);
+        }
+        if (value < node.data) {
+            node.left = insertNode(node.left, value);
+        } else if (value > node.data) {
+            node.right = insertNode(node.right, value);
+        }
+        return node;
+    }
+
+    void inorder() {
+        System.out.print("Inorder: ");
+        inorderNode(root);
+        System.out.println();
+    }
+
+    void inorderNode(TreeNode node) {
+        if (node != null) {
+            inorderNode(node.left);
+            System.out.print(node.data + " ");
+            inorderNode(node.right);
+        }
+    }
+
+    void preorder() {
+        System.out.print("Preorder: ");
+        preorderNode(root);
+        System.out.println();
+    }
+
+    void preorderNode(TreeNode node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorderNode(node.left);
+            preorderNode(node.right);
+        }
+    }
+
+    void postorder() {
+        System.out.print("Postorder: ");
+        postorderNode(root);
+        System.out.println();
+    }
+
+    void postorderNode(TreeNode node) {
+        if (node != null) {
+            postorderNode(node.left);
+            postorderNode(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BST b = new BST();
+        b.insert(40);
+        b.insert(20);
+        b.insert(60);
+        b.insert(10);
+        b.insert(30);
+        b.insert(50);
+        b.insert(70);
+
+        b.inorder();    // Expected: 10 20 30 40 50 60 70
+        b.preorder();   // Expected: 40 20 10 30 60 50 70
+        b.postorder();  // Expected: 10 30 20 50 70 60 40
+    }
+}
---
+++ src/main/java/sjb/mca/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package sjb.mca.tree;
+
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int value) {
+        data = value;
+        left = right = null;
+    }
+}
